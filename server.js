f32b5cb3a8f6a6af682da8674e74c5118b82cc35
Paul <git@github.com>
2024-12-09 20:19:27 +0300

    Updated Cors

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const jsonServer = require("json-server");
+const cors = require("cors"); // Import cors package
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
+// Enable CORS for all routes
+server.use(cors()); // used to enable CORS
 server.use(middlewares);
+
+// Custom route example (optional)
 server.use(router);
-server.listen(3001, () => {
-  console.log("JSON Server is running");
+
+const PORT = 3001;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
 });
